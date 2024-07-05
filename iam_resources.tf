7acbd3f11a2c3000762e1d2642b043d6ae8cde01
Rennard Watkins <rennard.watkins@gmail.com>
2024-07-05 16:20:38 -0400

    updated instance profile

--- iam_resources.tf
+++ iam_resources.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "instance-profile" {
+resource "aws_iam_role" "iam-role" {
   name               = var.aws_iam_role
   assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
 }
@@ -15,17 +15,17 @@ data "aws_iam_policy_document" "instance_assume_role_policy" {
 
 resource "aws_iam_instance_profile" "instance-profile" {
   name = var.aws_iam_role
-  role = aws_iam_role.instance-profile.name
+  role = aws_iam_role.iam-role.name
 }
 
 # Attach the AmazonDynamoDBFullAccess policy
 resource "aws_iam_role_policy_attachment" "dynamodb_full_access" {
-  role       = aws_iam_role.S3DB1.name
+  role       = aws_iam_role.iam-role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
 
 # Attach the AmazonS3FullAccess policy
 resource "aws_iam_role_policy_attachment" "s3_full_access" {
-  role       = aws_iam_role.S3DB1.name
+  role       = aws_iam_role.iam-role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
